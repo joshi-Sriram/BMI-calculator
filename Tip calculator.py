3b6966c2ca16647e525eeae715087452f1056cf1
joshi-Sriram <163913946+joshi-Sriram@users.noreply.github.com>
2024-05-14 20:01:55 +0530

    Update Tip calculator.py

--- Tip calculator.py
+++ Tip calculator.py
@@ -2,5 +2,6 @@ print("Welcome to Tip calculator")
 bill = int(input("enter your total bill: "))
 Tip = int(input("what persentage tip would like to give: "))
 persons= int(input("how many members are there to split the bill: "))
-amount = (bill + (bill*(Tip/100))) // persons
-print(f"each person should Pay {amount} ruppes")
+amount = (bill + (bill*(Tip/100))) / persons
+final_amount = "{:.2f}".format(amount)
+print(f"each person should Pay {final_amount} ruppes")
